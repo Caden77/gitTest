153808940a5b976b104b7a37a91e938b2de766fb
Caden77 <cadenbrinkman@gmail.com>
2024-04-30 10:21:32 -0600

    completed git test files

---
+++ gitScript1.sh
@@ -0,0 +1,24 @@
+git branch
+git init
+git branch
+git status
+git branch -m master main
+git status
+git add helloworld.py
+git status
+git commit -m "adding hello world for part A3"
+git branch branch1
+git branch
+git checkout branch1
+ls
+git add git_check.py
+git commit -m "adding git_check.py for part A5"
+git status
+git branch
+git checkout main
+git branch
+git merge branch1
+git status
+git diff branch1
+ls
+ls -a
---
+++ gitScript2.sh
@@ -0,0 +1,12 @@
+cd repo2
+ls
+git clone https://github.com/BYUComputingBootCampTests/git-clone.git
+ls
+cd git-clone/
+ls
+git log
+git diff b1cab
+git checkout b1cab door.py
+git status
+git commit -m "restored previous door.py file for part B5"
+
